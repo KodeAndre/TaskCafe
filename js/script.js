27eae19779cb5d5e22350ed4ecdcd25e256a18af
KodeAndre <andre.kodehode@gmail.com>
2021-05-26 10:50:08 +0200

    v0.0.11

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Initialize and add the map
+function initMap() {
+    // The location of Bergen
+    const cafe = { lat: 60.39581788898305, lng: 5.329053670870654 };
+    // The map, centered at Bergen
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 19,
+      center: cafe,
+    });
+    // The marker, positioned at Bergen
+    const marker = new google.maps.Marker({
+      position: cafe,
+      map: map,
+    });
+  }
